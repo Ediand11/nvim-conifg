fbc9ef1e59636ba4d0c88eef34a9c97686824316
“Ediand11” <ediand11@yandex.ru>
2025-09-23 21:06:24 +0300

    add config for navigate words

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -6,6 +6,24 @@ vim.keymap.set("n", "<C-M-Down>", ":resize -5<CR>", { noremap = true, silent = t
 vim.keymap.set("n", "<C-M-Left>", ":vertical resize -5<CR>", { noremap = true, silent = true })
 vim.keymap.set("n", "<C-M-Right>", ":vertical resize +5<CR>", { noremap = true, silent = true })
 
+if vim.g.vscode then
+  local opts = { noremap = true, silent = true }
+
+	local mappings = {
+		-- Word Navigation
+		{ 'n', 'w', 'cursorWordPartRight' },
+		{ 'n', 'b', 'cursorWordPartLeft' },
+		{ 'v', 'w', 'cursorWordPartRightSelect' },
+		{ 'v', 'b', 'cursorWordPartLeftSelect' },
+	}
+
+	for _, mapping in ipairs(mappings) do
+		local mode, key, command = mapping[1], mapping[2], mapping[3]
+
+		vim.keymap.set(mode, key, function() vim.fn.VSCodeNotify(command) end, opts)
+	end
+end
+
 vim.keymap.set("i", "<C-M-Right>", function()
   require("blink.cmp").show()
 end, { desc = "Принудительно открыть автодополнение (blink.cmp)" })
